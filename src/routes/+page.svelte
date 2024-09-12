75c900dad71fa50f0136aca711690b5cefd27900
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2024-09-12 15:09:06 -0400

    feat: link reading

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { page } from "$app/stores";
+
+  let formattedLinks: Array<Link>;
+  $: {
+    if (formattedLinks == null) formattedLinks = [];
+
+    const links = $page.url.searchParams.get("links");
+    if (links) {
+      links.split(",").forEach((set) => {
+        const split = set.split("*");
+        if (split.length === 2) {
+          formattedLinks.push({ href: split[1], text: split[0] });
+        }
+      });
+    }
+  }
+
+  interface Link {
+    href: string;
+    text: string;
+  }
+</script>
+
+<ul>
+  {#each formattedLinks as link}
+    <li>
+      <a href={link.href}>
+        {link.text}
+      </a>
+    </li>
+  {/each}
+</ul>
