4f17a7c19335bba21518902c73feaa9afe29bc80
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2024-09-12 22:43:44 -0400

    feat(style): animated buttons

--- src/lib/components/Link.svelte
+++ src/lib/components/Link.svelte
@@ -17,10 +17,26 @@
   <div bind:this={background} class="absolute left-2 top-2 w-72 border-2 border-gray-500 bg-red-800" />
   <a {href} class="">
     <div
-      class="absolute left-0 top-0 flex items-center justify-center w-72 border-2 border-gray-500 bg-white px-3 py-2 text-center"
+      class="absolute left-0 top-0 flex items-center justify-center w-72 border-2 border-gray-500 bg-white px-3 py-2 text-center go-back"
       bind:this={link}
     >
       <slot />
     </div>
   </a>
 </div>
+
+<style>
+  .go-back {
+    transition: transform 0.25s;
+  }
+  
+  .go-back:hover {
+    transition: transform 0.25s;
+    transform: translate(0.2rem, 0.2rem);
+  }
+  
+  .go-back:active {
+    transition: transform 0.1s;
+    transform: translate(0.5rem, 0.5rem);
+  }
+</style>
